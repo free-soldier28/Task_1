704c178664030455126fa82fffaa7887e607a6b9
romik280h <romik280h@gmail.com>
2017-10-30 01:24:44 +0300

    Реализовал метод поиска автомобилей по заданному диапазону скорости.

--- TaxiStation/TaxiStation/Autopark.cs
+++ TaxiStation/TaxiStation/Autopark.cs
@@ -23,5 +23,20 @@ namespace TaxiStation
             cars.Sort((car1, car2) => car1.FuelConsumption.CompareTo(car2.FuelConsumption));
             return cars;
         }
+
+        public List<Car> SearchCarsBySpeed(List<Car> cars, int min, int max)
+        {
+            List<Car> carSelection = new List<Car>();
+
+            foreach (Car car in cars)
+            {
+                if (car.MaxSpeed >= min && car.MaxSpeed <= max)
+                {
+                    carSelection.Add(car);
+                }
+            }
+
+            return carSelection;
+        }
     }
 }
